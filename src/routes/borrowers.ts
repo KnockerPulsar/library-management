c502e722c507410e70d21967ffa37d07bec1742d
Tarek Yasser <tarekspeed7@gmail.com>
2023-12-18 04:51:40 +0200

    Add OpenAPI documentation for borrower endpoints.

--- src/routes/borrowers.ts
+++ src/routes/borrowers.ts
@@ -1,14 +1,35 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Borrower:
+ *       type: object
+ *       required:
+ *         - id
+ *         - name
+ *         - email
+ *       properties:
+ *         id:
+ *           type: number
+ *           description: The id of the borrower, starts at 0 and is automatically incremented
+ *         name:
+ *           type: string
+ *           description: The name of the borrower
+ *         email:
+ *           type: string
+ *           description: The email of the borrower, must be unique
+ *       example:
+ *         id: 0
+ *         name: F. Irst Euser
+ *         email: first@euser.org
+ */
+
 import { Router, Request, Response } from 'express';
 import { ModelStatic, Model } from 'sequelize';
 import validator from 'email-validator';
 import { borrowerIdExists, errorHandler } from '../utils';
 
 type Borrower = ModelStatic<Model<any, any>>;
-// Operations:
-// 	- Add				POST: 		/borrowers
-// 	- Update			PATCH: 		/borrowers
-// 	- Delete			DELETE:		/borrowers
-// 	- List all			GET:		/borrowers, no body
 
 async function emailExists(Borrower: Borrower, email: string): Promise<boolean> {
    return (await Borrower.findOne({ where: { email }})) != null; 
@@ -24,6 +45,50 @@ async function checkEmail(Borrower: Borrower, email: string) {
 export default (Borrower: Borrower) => {
     const router = Router();
 
+    /**
+     * @swagger
+     * tags:
+     *   name: Borrowers
+     *   description: The borrower managing API
+     * /borrower:
+     *   post:
+     *     summary: Create a new borrower
+     *     tags: [Borrowers]
+     *     requestBody:
+     *       required: true
+     *       content:
+     *         application/json:
+     *           schema:
+     *             $ref: '#/components/schemas/Borrower'
+     *     responses:
+     *       201:
+     *         description: The created borrower.
+     *         content:
+     *           application/json:
+     *             schema:
+     *               $ref: '#/components/schemas/Borrower'
+     *       400:
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 message:
+     *                   type: string
+     *                   enum:
+     *                     - Invalid parameters
+     *                     - Invalid email
+     *                     - Email already exists
+     *       500:
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 message:
+     *                   type: string
+     *                   example: Server error!
+     */
     router.post('/', async (request: Request, response: Response) => {
 	const { name, email } = request.body;
 
@@ -44,10 +109,60 @@ export default (Borrower: Borrower) => {
 	    return;
 	}
 
-	await Borrower.create({ name, email });
-	response.status(201).send({ message: "Borrower added successfully" });
+	const borrower = { name, email };
+	await Borrower.create(borrower);
+	response.status(201).send({ name, email });
     });
 
+    /**
+     * @swagger
+     * tags:
+     *   name: Borrowers
+     *   description: The borrower managing API
+     * /borrower:
+     *   patch:
+     *     summary: Update a borrower
+     *     tags: [Borrowers]
+     *     requestBody:
+     *       required: true
+     *       content:
+     *         application/json:
+     *           schema:
+     *             type: object
+     *             properties:
+     *               name:
+     *                 type: string
+     *               email:
+     *                 type: string
+     *     responses:
+     *       200:
+     *         description: The updated borrower.
+     *         content:
+     *           application/json:
+     *             schema:
+     *               $ref: '#/components/schemas/Borrower'
+     *       400:
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 message:
+     *                   type: string
+     *                   enum:
+     *                     - Missing request parameters 
+     *                     - Invalid email
+     *                     - Email does not exist
+     *       500:
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 message:
+     *                   type: string
+     *                   example: Server error!
+     */
     router.patch('/', async(request: Request, response: Response) => {
 	const { name, email } = request.body;
 
@@ -68,13 +183,61 @@ export default (Borrower: Borrower) => {
 	    return;
 	}
 
-	Borrower.update(
+	const [_rowsUpdated, ...changed] = await Borrower.update(
 	    { name },
-	    { where: { email } }
+	    { where: { email }, returning: true }
 	);
-	response.status(201).send({ message: "Borrower added successfully" });
+	response.status(200).send(changed[0]);
     })
 
+    /**
+     * @swagger
+     * tags:
+     *   name: Borrowers
+     *   description: The borrower managing API
+     * /borrower:
+     *   delete:
+     *     summary: Delete a borrower
+     *     tags: [Borrowers]
+     *     requestBody:
+     *       required: true
+     *       content:
+     *         application/json:
+     *           schema:
+     *             type: object
+     *             properties:
+     *               email:
+     *                 type: string
+     *     responses:
+     *       200:
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 message:
+     *                   type: string
+     *                   example: Borrower deleted successfully
+     *       400:
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 message:
+     *                   type: string
+     *                   enum:
+     *                     - Email does not exist
+     *       500:
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 message:
+     *                   type: string
+     *                   example: Server error!
+     */
     router.delete('/', async(request: Request, response: Response) => {
 	const { borrowerId } = request.body;
 
@@ -84,9 +247,50 @@ export default (Borrower: Borrower) => {
 	}
 
 	Borrower.destroy({ where: { id: borrowerId } });
-	response.status(200).send({ message: "Borrower deleted successfully" });
+	response.status(204).send({ message: "Borrower deleted successfully" });
     });
 
+    /**
+     * @swagger
+     * tags:
+     *   name: Borrower
+     *   description: The borrower managing API
+     * /borrowers:
+     *   get:
+     *     summary: Get the data of all borrowers
+     *     tags: [Borrowers]
+     *     responses:
+     *       200:
+     *         description: The data of all borrowers
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: array
+     *               items:
+     *                 type: object
+     *                 properties:
+     *                   id:
+     *                     type: number
+     *                   name:
+     *                     type: string
+     *                   email:
+     *                     type: string
+     *                   createdAt:
+     *                     type: string
+     *                     format: date-time
+     *                   updatedAt:
+     *                     type: string
+     *                     format: date-time
+     *       500:
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               properties:
+     *                 message:
+     *                   type: string
+     *                   example: Server error!
+     */
     router.get('/', async (_: Request, response: Response) => {
 	response.status(200).send(await Borrower.findAll());
     });
